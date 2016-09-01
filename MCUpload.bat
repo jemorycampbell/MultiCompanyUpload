13553a6465f56649142390c46740aff956d29d3e
Jason E. Campbell <jason.campbell@gmail.com>
2016-09-01 20:49:09 +0000

    Initial commit to GitHub

---
+++ MCUpload.bat
@@ -0,0 +1,5 @@
+﻿@ECHO OFF
+PowerShell.exe -version 2 -NoProfile -ExecutionPolicy Bypass -Command "& 'C:\Exerve\MultiCompanyUpload\MCUpload.ps1' %1"
+rem PowerShell.exe -version 2 -NoProfile -ExecutionPolicy Bypass -Command "& 'C:\Exerve\MultiCompanyUpload\MCUpload.ps1' SU"
+rem %= Replace with path to script on production server -> PowerShell.exe -version 2 -NoProfile -ExecutionPolicy Bypass -Command "& 'C:\Exerve\MultiCompanyUpload\MCUpload.ps1'" =%
+PAUSE
